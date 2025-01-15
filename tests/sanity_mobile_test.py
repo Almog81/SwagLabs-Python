ac1c29c5d7b1eb95225457e974d9f8737a7ded30
Almog81 <almog81@gmail.com>
2025-01-15 13:57:05 +0200

    Update names of class "TestWebScenarios" and "TestSanityScenarios"

--- tests/sanity_mobile_test.py
+++ tests/sanity_mobile_test.py
@@ -1,9 +1,8 @@
-from utilities.common_ops_mobile import *
 from utilities.manage_mobile_pages import *
 
 login_data_list = read_from_json("loginData.json")
 
-class TestSanityScenarios:
+class TestMobileScenarios:
     @pytest.mark.parametrize("login_data", login_data_list)
     def test_01_login_all_type_of_users(self, pages, login_data):
         pages.login_page.login_all_users(login_data)
--- tests/sanity_web_test.py
+++ tests/sanity_web_test.py
@@ -2,9 +2,9 @@ from utilities.manage_pages import *
 
 login_data_list = read_from_json("loginData.json")
 
-class TestSanityScenarios:
+class TestWebScenarios:
     def test_01_login_page_logo(self, pages):
-        pages.home_page.navi_to_homepage()
+        pages.login_page.safe_login()
         assert pages.home_page.is_logo_displayed(), "Logo is not displayed"
 
     @pytest.mark.parametrize("login_data", login_data_list)
